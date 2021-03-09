7b37362dc8b32669b1a5da76f663452fd03e0de7
Kevin Gonzalez <32605942+KevinAlexandro@users.noreply.github.com>
2021-03-09 11:44:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Bienvenido a Not Instagram v1.0001");
+    response.end("Bienvenido a Not Instagram v1.0002");
 });
 
 const port = process.env.PORT || 1337;
