372870d44cb681ba81ad7c0c659b1e5636abd810
mauroheroes <64510772+mauroheroes@users.noreply.github.com>
2021-09-29 11:06:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Bienvenido a Not Instagram v1.0002");
+    response.end("Bienvenido a Not Instagram v5.0");
 });
 
 const port = process.env.PORT || 1337;
