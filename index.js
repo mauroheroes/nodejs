d626bb17af603d5f75288257ae8e6183c988fc16
mauroheroes <64510772+mauroheroes@users.noreply.github.com>
2022-01-27 00:23:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Bienvenido a Not Intagram v3.0");
+    response.end("Bienvenido a Not Intagram v2.1");
 });
 
 const port = process.env.PORT || 1337;
